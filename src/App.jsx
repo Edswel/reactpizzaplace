0a474881e47a9ca88a362e1010a8d8046c2c0657
Edswel <franckini@yahoo.com>
2024-03-27 12:58:41 +0100

    Fetch data using React Router "loader"

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, {loader as menuLoader} from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
@@ -18,7 +18,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/menu',
-        element: <Menu />
+        element: <Menu />,
+        loader: menuLoader
       },
       {
         path: '/cart',
