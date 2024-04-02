312857445b334d7f6fd84397da1435c1a4817fb3
Edswel <franckini@yahoo.com>
2024-04-02 14:25:45 +0100

    Add functionality to add menu item to cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Button({children, disabled, to, type}) {
+function Button({children, disabled, to, type, onClick}) {
   const base = "bg-yellow-400 text-sm uppercase font-semibold text-stone-800 inline-block tracking-widest rounded hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
 
   const styles = {
@@ -13,6 +13,11 @@ function Button({children, disabled, to, type}) {
   if (to) return (
     <Link to={to} className={styles[type]}>{children}</Link>
   )
+
+  if (onClick) return (
+    <button onClick={onClick} className={styles[type]} disabled={disabled}>{children}</button>
+  )
+  
   return (
     <button className={styles[type]} disabled={disabled}>{children}</button>
   )
