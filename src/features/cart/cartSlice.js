9531f40849a31d271ad71fc026f1dd5e73078ce1
Edswel <franckini@yahoo.com>
2024-04-02 17:29:01 +0100

    Add functionality to delete item from cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,15 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-  cart: [
-    // {
-    //   pizzaId: 12,
-    //   name: 'Hawaiian',
-    //   quantity: 2,
-    //   unitPrice: 16,
-    //   totalPrice: 32
-    // }
-  ]
+  cart: []
 }
 
 const cartSlice = createSlice({
@@ -49,3 +41,5 @@ export const getCart = state => state.cart.cart;
 export const getTotalCartQuantity = state => state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
 export const getTotalCartPrice = state => state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = id => state => state.cart.cart.find(item => item.pizzaId === id)?.quantity ?? 0;
