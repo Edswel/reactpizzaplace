0aca5ccb9ed96a5e4e332258073a389e3eb0dfce
Edswel <franckini@yahoo.com>
2024-04-02 12:03:42 +0100

    Read and update user state using redux

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Username() {
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
   return (
-    <div className='text-sm font-semibold hidden md:block'>Ini</div>
+    <div className='text-sm font-semibold hidden md:block'>{username}</div>
   )
 }
 
